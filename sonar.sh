ba0c01c1ecc3f33173bab7cc5af845197c3b27ce
John Heintz <john@gistlabs.com>
2010-06-11 10:15:08 -0500

    tune coverage setup

--- sonar.sh
+++ sonar.sh
@@ -3,5 +3,4 @@
 rm -rf src/gen-java
 rm -rf build/
 
-ant codecoverage -Dcobertura.dir=`pwd`/../cobertura-1.9.1/
-mvn --file=pom-sonar.xml sonar:sonar
+ant clean codecoverage -Dcobertura.dir="`pwd`/../cobertura-1.9.4.1/" && mvn --file=pom-sonar.xml sonar:sonar
